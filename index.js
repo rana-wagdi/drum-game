81a6b149ce12d79d5ea20a4266597b87de230d84
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-11 09:38:49 -0800

    drum game

---
+++ index.js
@@ -0,0 +1,49 @@
+var numberOfButton= document.querySelectorAll('.drum').length
+
+for (i=0 ; i < numberOfButton ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHtml=this.innerHTML;
+
+            switch (buttonInnerHtml) {
+              case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+
+              case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+              case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+              case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+              case "j":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+
+              case "k":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+
+              case "l":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+              
+              default:
+            }
+
+      //what to do when click detected
+    });
+}
+
